6e6de55b13bfeb825efd1b882ebf762a8e4ec0f9
mikecalderon742-cell <mikecalderon742@gmail.com>
2026-02-05 19:04:49 -0800

    Add upgrade route Resolve merge conflict

--- app.py
+++ app.py
@@ -4,7 +4,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Flask is running ✅</h1>"
+    return "<h1>Home page ✅</h1><a href='/upgrade'>Upgrade</a>"
+
+@app.route("/upgrade")
+def upgrade():
+    return "<h1>Upgrade page works ✅</h1>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
+
